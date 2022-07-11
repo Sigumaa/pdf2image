4685346ae24427bcaf502a50557eacc50cc90996
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-07-11 18:07:38 +0900

    feat: pdf to jpg

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-import "github.com/gen2brain/go-fitz"
+import (
+	"fmt"
+	"image/jpeg"
+	"os"
+	"path/filepath"
 
-func main() {}
+	"github.com/gen2brain/go-fitz"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		panic("The number of arguments does not match.")
+	}
+
+	pdfName := os.Args[1]
+	imgName := os.Args[2]
+
+	doc, err := fitz.New(pdfName)
+	if err != nil {
+		panic(err)
+	}
+
+	for n := 0; n < doc.NumPage(); n++ {
+		img, err := doc.Image(n)
+		if err != nil {
+			panic(err)
+		}
+
+		f, err := os.Create(filepath.Join(fmt.Sprintf("%s%03d.jpg", imgName, n)))
+		if err != nil {
+			panic(err)
+		}
+
+		err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
+		if err != nil {
+			panic(err)
+		}
+
+		f.Close()
+	}
+}
